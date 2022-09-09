5fdbec6a1f76076f69b3b4d4ff82bf379d5ffe96
fgorordo <flgorordo@gmail.com>
2022-09-09 16:01:04 -0300

    mui-day-1.5

---
+++ src/components/common/GridWrapper/styles.js
@@ -0,0 +1,6 @@
+export const gridWrapperStyles = {
+  backgroundColor: '#eaeff1',
+  padding:'48px 32px',
+  minHeight:'calc(100vh - 166px)',
+  position:'relative',
+}
--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,21 +1,67 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import Header from '../../components/Header/Header';
+import BasicCard from '../../components/common/BasicCard/BasicCard';
+import SearchBar from '../../components/common/SearchBar/SearchBar';
+import CommonButton from '../../components/common/CommonButton/CommonButton'
+import { IconButton, Typography } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import { cardHeaderStyles } from './styles';
+import { Box } from '@mui/system';
+import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
 
 
 
 
 const Authentication = () => {
-  return (
-    <Grid
-      item
-      sx={{marginLeft:'320px'}}
-      xl={10}
-      md={10}
-      sm={10}
+  const getSearchBar = () => {
+    const handleChange = (value) => {
+      console.log(value)
+    };
 
+    const addUser = () => {
+      console.log('click')
+    };
+
+    return (
+       <Box sx={cardHeaderStyles.wrapper}>
+                <SearchBar 
+                    placeholder="Search by email address, phone number, or user UID"
+                    onChange={(event) => handleChange(event.target.value)}
+                    searchBarWidth='720px'
+                />
+                <Box>
+                    <CommonButton 
+                        variant="contained"
+                        onClick={addUser}
+                        size="large"
+                        sx={cardHeaderStyles.addUserButton}
+                    >
+                        Add user
+                    </CommonButton>
+                    <IconButton>
+                        <RefreshIcon />
+                    </IconButton>
+                </Box>
+            </Box>
+    )
+  };
+
+  const getContent = () => (
+    <Typography
+      align="center"
+      sx={{margin: '40px 16px', color: 'rgba(0,0,0,0.6)', fontSize: '1.3rem'}}
     >
-    </Grid>
+      No users for this project
+    </Typography>
+  );
+
+  return (
+    <GridWrapper>
+      <BasicCard 
+        header={getSearchBar()}
+        content={getContent()}
+      />
+    </GridWrapper>
   )
 };
 
